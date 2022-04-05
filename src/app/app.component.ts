f181b0f69fa27005b1a16a57e538335063103473
1214741 <j98fer@gmail.com>
2022-04-05 00:01:29 -0500

    3 columnas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ViveroService } from '../app/services/vivero.service'
+import { ViveroService } from '../app/services/vivero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'chopevivero-frontend';
-  
-  constructor(private viveroService: ViveroService){
-    console.log("Componente Creado")
+  temperatura: any = [];
+  humedad: any = [];
+  radiacion: any = [];
+
+  constructor(private viveroService: ViveroService) {
+    console.log('Componente Creado');
   }
 
   ngOnInit(): void {
-    console.log("Componente inicializado")
+    console.log('Componente inicializado');
+    this.viveroService
+      .getTemperatura()
+      .subscribe((response: any) => this.temperatura = response);
+    this.viveroService
+      .getHumedad()
+      .subscribe((response: any) => this.humedad = response);
+    this.viveroService
+      .getRadiacion()
+      .subscribe((response: any) => this.radiacion = response);
   }
 }
--- src/app/services/vivero.service.ts
+++ src/app/services/vivero.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ViveroService {
+  constructor(private http: HttpClient) {
+    console.log('Servicio Http');
+  }
 
-  constructor(private http: HttpClientModule) {
-    console.log("Servicio Http")
-   }
+  getTemperatura(): any {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+  getHumedad(): any {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+  getRadiacion(): any {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
 }
