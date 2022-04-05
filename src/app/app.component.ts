f920176b59f027fbcf021b39fce4903aa463cd1b
1214741 <j98fer@gmail.com>
2022-04-04 19:27:07 -0700

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViveroService } from '../app/services/vivero.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'chopevivero-frontend';
+  
+  constructor(private viveroService: ViveroService){
+    console.log("Componente Creado")
+  }
+
+  ngOnInit(): void {
+    console.log("Componente inicializado")
+  }
 }
