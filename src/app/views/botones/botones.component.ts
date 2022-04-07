f54f8bc1a78923a344f60073212d37c1d93518f4
1214741 <j98fer@gmail.com>
2022-04-06 23:29:28 -0500

    boton

--- src/app/views/botones/botones.component.ts
+++ src/app/views/botones/botones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-botones',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BotonesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
+
+  MostrarTemperatura(){
+    //console.log(1)
+    this.renderer.setAttribute(this., "display", "flex");
+  }
+  MostrarHumedad(){
+    console.log(2)
+  }
+  MostrarRadiacion(){
+    console.log(3)
+  }
 
   ngOnInit(): void {
   }
